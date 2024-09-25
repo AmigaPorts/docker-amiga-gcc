dbb0d28bc9ec00959fbb293711ab7b315569133c
Marlon Beijer <marlon@eevul.org>
2024-09-25 23:01:37 +0200

    Fix building of ppc-amigaos image + add arm64 version of image

--- platforms/ppc-amigaos/prep.sh
+++ platforms/ppc-amigaos/prep.sh
@@ -6,8 +6,8 @@ set -e
 #set compiler params
 export TARGET='ppc-amigaos'
 export SYSROOT=/opt/${TARGET}/usr
-export M68K_CPU="-mhard-float"
-export M68K_COMMON="-s -ffast-math -fomit-frame-pointer -I${SYSROOT}/include -L${SYSROOT}/lib"
+export M68K_CPU=""
+export M68K_COMMON="-s -athread=single -ffast-math -fomit-frame-pointer -I${SYSROOT}/include -L${SYSROOT}/lib"
 export M68K_CFLAGS="${CFLAGS} ${M68K_CPU} ${M68K_COMMON}"
 export M68K_CXXFLAGS="${CXXFLAGS} ${M68K_CPU} ${M68K_COMMON}"
 export CURPATH="${PWD}"
@@ -28,25 +28,39 @@ mkdir -p "${SUBMODULES}"/MiniGL
 cd "${SUBMODULES}"/MiniGL
 wget http://os4depot.net/share/driver/graphics/minigl.lha -O minigl.lha
 lha -x minigl.lha
-mkdir -p ${SYSROOT}/usr/include
-mkdir -p ${SYSROOT}/usr/lib
+mkdir -p ${SYSROOT}/include
+mkdir -p ${SYSROOT}/lib
 cp -fvr MiniGL/SDK/local/common/include/* ${SYSROOT}/include/
 cp -fvr MiniGL/SDK/local/newlib/lib/* ${SYSROOT}/lib/
 cd "${SUBMODULES}"
 
-
 # SDL1.2
-if [ ! -d "${SUBMODULES}/SDL" ]; then
-	git clone https://github.com/AmigaPorts/SDL.git "${SUBMODULES}"/SDL
-fi
+mkdir -p "${SUBMODULES}"/SDL
 cd "${SUBMODULES}"/SDL
-git checkout SDL-1.2-AmigaOS3
-git pull
-rm -rf "${SUBMODULES}"/SDL/build
-mkdir -p "${SUBMODULES}"/SDL/build
-cd "${SUBMODULES}"/SDL/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DTOOLCHAIN_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
-cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
+wget https://github.com/AmigaPorts/SDL-1.2/releases/download/v1.2.16-release-amigaos4/SDL.lha -O SDL.lha
+lha -x SDL.lha
+mkdir -p ${SYSROOT}/include
+mkdir -p ${SYSROOT}/lib
+cp -fvr SDL/SDK/local/newlib/include/* ${SYSROOT}/include/
+cp -fvr SDL/SDK/local/newlib/lib/* ${SYSROOT}/lib/
+
+# SDL2
+#if [ ! -d "${SUBMODULES}/SDL" ]; then
+#	git clone https://github.com/AmigaPorts/SDL.git "${SUBMODULES}"/SDL
+#fi
+mkdir -p "${SUBMODULES}"/SDL2
+cd "${SUBMODULES}"/SDL2
+wget https://github.com/AmigaPorts/SDL/releases/download/v2.30.5-amigaos4/SDL2.lha -O SDL2.lha
+lha -x SDL2.lha
+mkdir -p ${SYSROOT}/include
+mkdir -p ${SYSROOT}/lib
+cp -fvr SDL2/SDK/local/newlib/include/* ${SYSROOT}/include/
+cp -fvr SDL2/SDK/local/newlib/lib/* ${SYSROOT}/lib/
+#git pull
+#make -f Makefile.amigaos4
+##rm -rf "${SUBMODULES}"/SDL-build
+##cmake -S"${SUBMODULES}"/SDL -B"${SUBMODULES}"/SDL-build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DHAVE_SDL_TIMERS=TRUE -DHAVE_SDL_THREADS=TRUE -DTOOLCHAIN_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++17"
+##cmake --build "${SUBMODULES}"/SDL-build --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
 cd "${SUBMODULES}"
 
 # Zziplib
